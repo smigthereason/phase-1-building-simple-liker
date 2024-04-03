a6ce56726fa188b089591db865e62350c8cc9cac
victor@victor <victor.dmaina@gmail.com>
2024-04-03 18:29:24 +0300

    add

--- main.js
+++ main.js
@@ -3,10 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeButtons = document.querySelectorAll('.like-glyph');
+const errorModal = document.querySelector('#modal');
+const errorMessage = document.querySelector('#modal-message');
 
+errorModal.classList.add('hidden');
 
+function toggleLike(e) {
+  const heart = e.target;
+  mimicServerCall()
+    .then(() => {
+      heart.classList.toggle('activated-heart');
+      if (heart.classList.contains('activated-heart')) {
+        heart.innerText = '♥';
+      } else {
+        heart.innerText = '♡';
+      }
+    })
+    .catch(() => {
+      errorMessage.innerText = 'Oops! Something went wrong.';
+      errorModal.classList.remove('hidden');
+      setTimeout(() => {
+        errorModal.classList.add('hidden');
+      }, 3000);
+    });
+}
 
-
+for (const button of likeButtons) {
+  button.addEventListener('click', toggleLike);
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
